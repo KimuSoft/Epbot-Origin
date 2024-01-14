df7f7508255d34d7db7faf99deee0a24c8db36a8
ingyu0413 <ingyu0413@naver.com>
2024-01-15 04:00:44 +0900

    fix: remove SlashCommandGroup "낚시카드"

--- cogs/fishing/__init__.py
+++ cogs/fishing/__init__.py
@@ -3,4 +3,4 @@ from config import SLASH_COMMAND_REGISTER_SERVER as SCRS
 
 fishing_group = SlashCommandGroup("낚시터", "낚시터 관련 명령어")
 land_group = fishing_group.create_subgroup("시설", "시설 관련 명령어")
-theme_group = SlashCommandGroup("낚시카드", "낚시카드 테마 관련 명령어")
+# theme_group = SlashCommandGroup("낚시카드", "낚시카드 테마 관련 명령어")
