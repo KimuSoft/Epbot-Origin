f3e43b2b10e17c7d7e218cbe2125920e5f4a4d89
KOI3125 <koi312500@gmail.com>
2025-04-14 19:16:23 +0900

    fix: created_at works with thread

--- cogs/fishing/fishing.py
+++ cogs/fishing/fishing.py
@@ -2,6 +2,7 @@
 from discord.commands import slash_command
 from discord.commands import Option
 from discord.ext import commands
+from discord import Thread
 import discord
 import os
 import ast
@@ -36,7 +37,10 @@ class InfoCog(commands.Cog):
         room = await Room.fetch(ctx.channel)
         fee_range = room.fee_range
         cleans = await room.get_cleans()
-        created_at = room.channel.created_at
+        if isinstance(room.channel, Thread):
+            created_at = room.channel.parent.created_at
+        else:
+            created_at = room.channel.created_at
         print(room.channel, created_at)
 
         # 기본 정보
